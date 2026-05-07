81b2c3d1a8f60c37e241375382377a24f4123cf3
Aruzhan38 <241372@astanait.edu.kz>
2026-05-08 00:05:02 +0500

    backup local generated files before merge

---
+++ booking-service/internal/config/config.go
@@ -0,0 +1,68 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/joho/godotenv"
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	DBURL      string
+	DBHost     string
+	DBUser     string
+	DBPassword string
+	DBName     string
+	DBPort     string
+	GRPCPort   string
+}
+
+func Load() *Config {
+	_ = godotenv.Load()
+
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	_ = viper.ReadInConfig()
+
+	dbURL := firstNonEmpty(
+		os.Getenv("DB_URL"),
+		os.Getenv("DATABASE_URL"),
+		viper.GetString("db.url"),
+		viper.GetString("database_url"),
+	)
+
+	if dbURL == "" {
+		dbURL = fmt.Sprintf(
+			"host=%s user=%s password=%s dbname=%s port=%s sslmode=require search_path=booking_service",
+			firstNonEmpty(os.Getenv("DB_HOST"), viper.GetString("db.host")),
+			firstNonEmpty(os.Getenv("DB_USER"), viper.GetString("db.user")),
+			firstNonEmpty(os.Getenv("DB_PASSWORD"), viper.GetString("db.password")),
+			firstNonEmpty(os.Getenv("DB_NAME"), viper.GetString("db.name")),
+			firstNonEmpty(os.Getenv("DB_PORT"), viper.GetString("db.port")),
+		)
+	}
+
+	return &Config{
+		DBURL:      dbURL,
+		DBHost:     firstNonEmpty(os.Getenv("DB_HOST"), viper.GetString("db.host")),
+		DBUser:     firstNonEmpty(os.Getenv("DB_USER"), viper.GetString("db.user")),
+		DBPassword: firstNonEmpty(os.Getenv("DB_PASSWORD"), viper.GetString("db.password")),
+		DBName:     firstNonEmpty(os.Getenv("DB_NAME"), viper.GetString("db.name")),
+		DBPort:     firstNonEmpty(os.Getenv("DB_PORT"), viper.GetString("db.port")),
+		GRPCPort:   firstNonEmpty(os.Getenv("GRPC_PORT"), viper.GetString("grpc.port"), "50053"),
+	}
+}
+
+func firstNonEmpty(values ...string) string {
+	for _, v := range values {
+		if v != "" {
+			return v
+		}
+	}
+	return ""
+}
